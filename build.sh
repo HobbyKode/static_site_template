d3b7b2fd874051d14c04a749a2729971e06b5f84
HobbyKode <speedemoniak@yahoo.fr>
2025-03-16 07:49:26 -0400

    Build.sh Updated

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ cd "$(dirname "$0")"
 # Define the GitHub repository name (replace with your actual repo)
 REPO_NAME="static_site_template"
 
+# Disable bytecode compilation (no __pycache__)
+export PYTHONDONTWRITEBYTECODE=1
+
 
 #Building the site for GitHub Pages
 echo  "🚀 Building site for GitHub Pages with basepath:  /$REPO_NAME/"
