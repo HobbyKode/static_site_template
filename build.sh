28cd7edb6dc275afc672ded1960ab1a9c9a1f99e
HobbyKode <speedemoniak@yahoo.fr>
2025-03-16 10:51:44 -0400

    Just sent my website to Brazil

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 # Ensure we're in the correct project root
 cd "$(dirname "$0")"
 
-# Define the GitHub repository name (replace with your actual repo)
-REPO_NAME="static_site_template"
+
 
 # Disable bytecode compilation (no __pycache__)
 export PYTHONDONTWRITEBYTECODE=1
 
 
 #Building the site for GitHub Pages
-echo  "🚀 Building site for GitHub Pages with basepath:  /$REPO_NAME/"
-python3 src/main.py "/$REPO_NAME/" 
+echo  "🚀 Building site for GitHub Pages with basepath:  /static_site_template/"
+python3 src/main.py "/static_site_template/" 
