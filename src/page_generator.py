94c712a111fbe726b44f5f43603e1cd41b827453
HobbyKode <speedemoniak@yahoo.fr>
2025-03-16 12:52:14 -0400

    Fixed basepath double slash issue

--- src/page_generator.py
+++ src/page_generator.py
@@ -57,8 +57,8 @@ def generate_page(from_path, template_path, dest_path, basepath):
     print(final_html[:500])  # Show first 500 characters for preview
     
     #Placeholders replacements for GitHub Pages basepath
-    final_html = final_html.replace('href="/', f'href="{basepath}')
-    final_html = final_html.replace('src="/', f'src="{basepath}')
+    final_html = final_html.replace('href="/', f'href="{basepath}/')
+    final_html = final_html.replace('src="/', f'src="{basepath}/')
 
     # Add right after your replacements
     print(f"DEBUG: Example link after replacement: {final_html.find('href=')}")
